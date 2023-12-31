858b5fc79a5d82230e45fa4e901d73ec37f4cccf
marcuspeh <hkmpeh@gmail.com>
2024-01-01 00:00:28 +0800

    Update test case

--- backend/src/services/helper/passwordServiceHelper.test.ts
+++ backend/src/services/helper/passwordServiceHelper.test.ts
@@ -6,17 +6,24 @@ import {errorCode} from '../../errors/errorCode';
 const mockSalt = '$2a$10$fHA6/UVmzntiaLlJDo49PO';
 
 jest.mock('bcryptjs', () => ({
-  genSalt: jest.fn().mockResolvedValue('$2a$10$fHA6/UVmzntiaLlJDo49PO'),
   hash: jest.fn().mockImplementation(jest.requireActual('bcryptjs').hash),
   compare: jest.fn().mockImplementation(jest.requireActual('bcryptjs').compare),
 }));
 
 describe('PasswordServiceHelper', () => {
   describe('hashPassword', () => {
+    afterEach(() => {
+      bcrypt.genSalt = jest.fn();
+    });
+
     test('success, number', async () => {
       const input = '35366746';
       const expectedOutput =
         '$2a$10$fHA6/UVmzntiaLlJDo49POBWprEEjz13t0cQ9IDEpt3WEkvNv6G4G';
+
+      bcrypt.genSalt = jest
+        .fn()
+        .mockResolvedValue('$2a$10$fHA6/UVmzntiaLlJDo49PO');
 
       const output = await passwordServiceHelper.hashPassword(input);
       expect(output).toBe(expectedOutput);
@@ -30,6 +37,10 @@ describe('PasswordServiceHelper', () => {
       const expectedOutput =
         '$2a$10$fHA6/UVmzntiaLlJDo49POtwr5qz6Jm12TEQqlPF/ob6SIWvCgid.';
 
+      bcrypt.genSalt = jest
+        .fn()
+        .mockResolvedValue('$2a$10$fHA6/UVmzntiaLlJDo49PO');
+
       const output = await passwordServiceHelper.hashPassword(input);
       expect(output).toBe(expectedOutput);
 
@@ -42,6 +53,10 @@ describe('PasswordServiceHelper', () => {
       const expectedOutput =
         '$2a$10$fHA6/UVmzntiaLlJDo49POGY1Z4yDkhhRFIs41Wy1w2JHqPXI4eci';
 
+      bcrypt.genSalt = jest
+        .fn()
+        .mockResolvedValue('$2a$10$fHA6/UVmzntiaLlJDo49PO');
+
       const output = await passwordServiceHelper.hashPassword(input);
       expect(output).toBe(expectedOutput);
 
@@ -53,6 +68,10 @@ describe('PasswordServiceHelper', () => {
       const input = '';
       const expectedOutput =
         '$2a$10$fHA6/UVmzntiaLlJDo49POyhlWEbEyAf5BFW/UJ6J9CzSBltJ3aae';
+
+      bcrypt.genSalt = jest
+        .fn()
+        .mockResolvedValue('$2a$10$fHA6/UVmzntiaLlJDo49PO');
 
       const output = await passwordServiceHelper.hashPassword(input);
       expect(output).toBe(expectedOutput);
